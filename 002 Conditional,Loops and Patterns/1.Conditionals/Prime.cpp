f6e94d94c10b254795b8d26ba6a40c270fe68ea4
Prajwal1110 <105029382+Prajwal1110@users.noreply.github.com>
2023-01-08 22:10:24 +0530

    Create Prime.cpp

---
+++ 002 Conditional,Loops and Patterns/1.Conditionals/Prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool oddorev(int x){
+   if(x%2==0){
+       return true;
+   }else{
+       return false;
+   }
+}
+
+int main() {
+	
+	int m;
+	cin>>m;
+	if(oddorev(m)){
+	    cout<<"Even no"<<endl;
+	}else{
+	    cout<<"Odd no"<<endl;
+	}
+	
+	return 0;
+}
+Footer
+© 2023 GitHub, Inc.
